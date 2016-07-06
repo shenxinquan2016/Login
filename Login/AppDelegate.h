afbae9f07d98ee3048cf77b1f0fc3b0c57f3504c
沈昕权 <shenxinquan123@msn.cn>
2016-07-06 09:00:12 +0800

    Initial Commit

---
+++ Login/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Login
+//
+//  Created by 沈昕权 on 16/7/6.
+//  Copyright © 2016年 沈昕权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Login/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Login
+//
+//  Created by 沈昕权 on 16/7/6.
+//  Copyright © 2016年 沈昕权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
